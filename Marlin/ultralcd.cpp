1e1eff2b5fd5c0f5a7d0c1722ef1584bc4c6d058
Bevaz <The.Bevaz@gmail.com>
2014-01-12 01:21:43 +0400

    Added home-z offset to LCD

--- Marlin/ultralcd.cpp
+++ Marlin/ultralcd.cpp
@@ -707,6 +707,7 @@ static void lcd_control_motion_menu()
 {
     START_MENU();
     MENU_ITEM(back, MSG_CONTROL, lcd_control_menu);
+    MENU_ITEM_EDIT(float3, MSG_ADD_HOMEING, &add_homeing[Z_AXIS], 0.1, 990);
     MENU_ITEM_EDIT(float5, MSG_ACC, &acceleration, 500, 99000);
     MENU_ITEM_EDIT(float3, MSG_VXY_JERK, &max_xy_jerk, 1, 990);
     MENU_ITEM_EDIT(float52, MSG_VZ_JERK, &max_z_jerk, 0.1, 990);
